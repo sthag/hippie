db96a298fb8bba79bf80d93a815a58680d1aab1e
Stephan Hagedorn <klarsichtweise@gmail.com>
2017-08-22 00:09:50 +0200

    Tweaks

--- code/variables.js
+++ code/variables.js
@@ -1,7 +1,7 @@
 var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
 var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
 
-var full_view_hover = true;
+var full_view_hover = false;
 
 var doc_pos_y = 0;
 var basic_ease = 600;
